a89535fedbdcc82ab9f67a7bcc537353ad2fd1e3
Jordan Carlin <jordanmcarlin@gmail.com>
2024-09-17 01:17:28 -0700

    Cleanup

--- fpga/src/counter.sv
+++ fpga/src/counter.sv
@@ -3,18 +3,18 @@ module counter #(parameter THRESHOLD = 1000) (
   output logic       clk_stb
 );
 
-  logic [31:0] counter;
+  logic [31:0] count;
 
 //   // Generate slower clock signal
   always_ff @(posedge clk)
-    clk_stb <= (counter == THRESHOLD-1'b1);
+    clk_stb <= (count == THRESHOLD-1'b1);
 
   // Toggle the slow clock when the counter reaches the threshold
   always_ff @(posedge clk) begin
     if (~reset | clk_stb)
-      counter <= 0;
+      count <= 0;
     else
-      counter <= counter + 1;
+      count <= count + 1;
   end
 
 endmodule
